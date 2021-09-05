77781ed0c3765c2fecf194105b74c8b998a080c6
Ihar Khakholka <igor.xoxolko@gmail.com>
2021-09-05 20:23:39 +0300

    +add MongoDB to API

--- api/app.py
+++ api/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, make_response
+import os
 
 from datasets.controllers import datasets
 from auth.auth import auth
 
-from extentions import cors, guard
+from extentions import cors, guard, mongo
 
 from lib.models import User
 
@@ -11,16 +12,17 @@ from lib.models import User
 server = Flask(__name__, static_url_path='')
 
 server.config["SECRET_KEY"] = "13221fa2r1wqrasr125"
+server.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
+mongo.init_app(server)
+cors.init_app(server)
+guard.init_app(server, User)
 
 
 server.register_blueprint(datasets, url_prefix = '/api/datasets')
-
 server.register_blueprint(auth, url_prefix = '/api/auth')
 
-cors.init_app(server)
 
-guard.init_app(server, User)
 
 @server.route('/api', methods=["GET"])
 def index():
--- api/extentions.py
+++ api/extentions.py
@@ -1,11 +1,11 @@
 
-
+from flask_pymongo import PyMongo
 import flask_praetorian
 import flask_cors
 
 from lib.access.authenticate import authenticate
 
-
+mongo = PyMongo()
 guard = flask_praetorian.Praetorian()
 cors = flask_cors.CORS()
 
